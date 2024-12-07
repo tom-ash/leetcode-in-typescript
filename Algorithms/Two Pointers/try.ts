dc3207442e827190dd840be65879ac715710db8b
Tomasz Bogusz <tomaszbogusz@outlook.com>
2024-12-07 08:16:24 +0100

    feat: add "Two Pointers" to Algorithms

---
+++ Algorithms/Two Pointers/try.ts
@@ -0,0 +1,9 @@
+// import { twoPointer } from ".";
+import { twoPointer } from "./Two\ Pointers";
+
+const args = process.argv.slice(2);
+const arr = eval(args[0]);
+
+// TODO: Add checks for arr.
+
+twoPointer(arr);
