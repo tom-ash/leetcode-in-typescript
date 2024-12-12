35c11d7abea1dd2ed7876fe4b67bab3177e731e2
Tomasz Bogusz <tomaszbogusz@outlook.com>
2024-12-12 17:08:22 +0100

    Remove algorithms

--- Algorithms/Two Pointers/try.ts
+++
@@ -1,9 +0,0 @@
-// import { twoPointer } from ".";
-import { twoPointer } from "./Two\ Pointers";
-
-const args = process.argv.slice(2);
-const arr = eval(args[0]);
-
-// TODO: Add checks for arr.
-
-twoPointer(arr);
